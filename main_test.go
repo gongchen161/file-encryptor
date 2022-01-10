3d00616ebe5f3db69b39f27e8028e32bbf0807d3
gongchen161 <gong.chen161@gmail.com>
2022-01-09 19:48:43 -0500

    Add unit test for bad password

--- main_test.go
+++ main_test.go
@@ -1,27 +1,54 @@
 package main
 
-import "testing"
+import (
+	"crypto/sha256"
+	"testing"
+)
 
-func TestEncryptionDecryption(t *testing.T) {
-	expectedResult := "This is a plain text for testing"
+const FILE_CONTENT = "This is a plain text for testing"
+const PASSWORD = "PASSWORD_TO_BE_HASHED_IN_SHA256"
 
-	plainFileContentByte := ([]byte)(expectedResult)
-	hashedPasswordByte := ([]byte)("SOME_RANDOM_HASH")
+func TestCorrectEncryptionDecryption(t *testing.T) {
 
-	encryptedTextByte, err := getCipherText(plainFileContentByte, hashedPasswordByte)
+	plainFileContentByte := ([]byte)(FILE_CONTENT)
+	hashedPasswordByte := sha256.Sum256([]byte(PASSWORD))
+
+	encryptedTextByte, err := getCipherText(plainFileContentByte, hashedPasswordByte[:])
 
 	if err != nil {
 		t.Error("Unexpected encryption error", err)
 	}
 
-	decryptedTextByte, err := getPlainText(encryptedTextByte, hashedPasswordByte)
+	decryptedTextByte, err := getPlainText(encryptedTextByte, hashedPasswordByte[:])
 
 	if err != nil {
 		t.Error("Unexpected decryption error", err)
 	}
 
-	if (string)(decryptedTextByte) != expectedResult {
-		t.Error("Incorrect results.\nExpected:", expectedResult, "\nActual:  ", (string)(decryptedTextByte))
+	if (string)(decryptedTextByte) != FILE_CONTENT {
+		t.Error("Incorrect results.\nExpected:", FILE_CONTENT, "\nActual:  ", (string)(decryptedTextByte))
+	}
+
+}
+
+func TestBadHashPasswordInDecryption(t *testing.T) {
+
+	plainFileContentByte := ([]byte)(FILE_CONTENT)
+	hashedPasswordByte := sha256.Sum256([]byte(PASSWORD))
+
+	encryptedTextByte, err := getCipherText(plainFileContentByte, hashedPasswordByte[:])
+
+	if err != nil {
+		t.Error("Unexpected encryption error", err)
+	}
+
+	// create an incorrect password in decryption
+	badHashedPasswordByte := sha256.Sum256([]byte("BAD_PASSWORD"))
+
+	_, err = getPlainText(encryptedTextByte, badHashedPasswordByte[:])
+
+	if err == nil {
+		t.Error("Error should not be nil. Wrong password used in decryption")
 	}
 
 }
